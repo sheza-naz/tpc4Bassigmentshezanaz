a2579be72da398e8427ca4ef9aa4eb1ecf3a2d98
sheza-naz <64827065+sheza-naz@users.noreply.github.com>
2020-07-14 00:57:36 +0500

    done 4B assignment
    completed

---
+++ Assigment4BSheza/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SplashScreen : MonoBehaviour
+{
+    int ScreenNumber;
+    public void penguin()
+    {
+        if (ScreenNumber == 0)
+        {
+            ScreenNumber = 1;
+            SceneManager.LoadScene(ScreenNumber);
+        }
+
+    }
+
+    public void humingBird()
+    {
+        if (ScreenNumber == 0)
+        {
+            ScreenNumber = 2;
+            SceneManager.LoadScene(ScreenNumber);
+        }
+
+    }
+
+}
